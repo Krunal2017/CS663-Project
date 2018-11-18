6ecf8d5753d0616f1b65a34d6915049803af6bba
bhatsukanya <sukanyab4@gmail.com>
2018-11-18 15:58:11 +0530

    retrieval precision and demo format

---
+++ src/ret_prec.m
@@ -0,0 +1,42 @@
+%% PRECISION COMPUTATION FOR RETRIEVAL
+
+function [prec] = ret_prec(score_struct_sorted, D, top_im_num, keyword, database_dir)
+    N = length(D);
+    Nr = top_im_num;
+    
+    true_total = 0;
+    ret_total = 0;
+    
+    D_struct = struct();
+    Dr_struct = struct();
+    
+    %Database Directory
+    dir_name = strcat(strcat(database_dir,keyword),'/');
+    
+    %Find Ground Truth
+    for i=1:N
+        c = strsplit(D(i).name, '.');
+        temp = cell2mat(c(1));
+        D_struct(i).name = temp;
+        
+        filename = strcat(strcat(dir_name, D_struct(i).name), '.png'); 
+        if(isfile(filename))
+            true_total = true_total + 1;
+        end
+    end   
+    
+    %Find correct retrieved images
+    for i=1:Nr
+        c = strsplit(score_struct_sorted(i).name, '.');
+        temp = cell2mat(c(1));
+        Dr_struct(i).name = temp;
+        
+        filename = strcat(strcat(dir_name, Dr_struct(i).name), '.png'); 
+        if(isfile(filename))
+            ret_total = ret_total + 1;
+        end
+    end   
+    
+    prec = (ret_total / true_total)*100;
+
+end
--- src/sbir.m
+++ src/sbir.m
@@ -1,100 +1,51 @@
-%% SBIR USING MHEC AND SOH
+%% SBIR FOR QUERY IMAGE
 tic;
 
 window_size = 8;
+top_im_num = 100;
+norm_thres = 0.5;
+keyword = 'Butterfly';
+database_dir = '../SampleImages/';
 
-% %Query image
-% q_im = imread('../SampleImages/CoffeMug/6.jpg');
-% [im, mask] = textureDistinctMap(q_im);
-% % figure(1), imshow(mat2gray(im));
-% % figure(2), imshow(mat2gray(mask));
-% 
-% [q_image, Ix, Iy, x, y] = featureExtraction(double(q_im),mask);
-% % figure,imshow(q_image),title('Query Image Salient points'), hold on, scatter(y,x,'filled','r');
-% 
-% q_h = soh(Ix, Iy, x, y, window_size);
+%Database Directory
+dir_name = strcat(database_dir,keyword);
 
-% Process the database
-D = dir('../../THUR15000/CoffeeMug/Src/*.jpg'); % check dir command, in matlab documentation
-score = zeros(length(D),1);
-N=floor(length(D)*0.8);
+%Query image
+q_im = imread(strcat(dir_name,'/10.jpg'));
+[im, mask] = textureDistinctMap(q_im);
+[q_image, Ix, Iy, x, y] = featureExtraction(double(q_im),mask); 
+q_h = soh(Ix, Iy, x, y, window_size);
 
-ADA_THRES_MASKS=[];
-SALIENCY_MAPS=[];
-SALIENCY_HISTOGRAMS=[];
-for i=1:N% for each file in the directory(1 and 2 are '.' and '..')
-     filename = strcat('../../THUR15000/CoffeeMug/Src/',D(i).name);
-     X=double(imread(filename));
-     [im,mask1] = textureDistinctMap(X);
-     if size(im,1)<800
-         a=800-size(im,1);
-     end
-     if size(im,2)<1028
-         b=1028-size(im,2);
-     end
-     padded_im=padarray(im,[a/2,b/2],0,'both');
-     padded_mask=padarray(mask1,[a/2,b/2],0,'both');
-     SALIENCY_MAPS(:,:,i)=padded_im;
-     ADA_THRES_MASKS(:,:,i)=padded_mask;
-     [image, Ix, Iy, x, y] = featureExtraction(double(X),mask1);
-     cx=x;
-     cy=y;
-%      figure,imshow(mat2gray(image)),title('Salient points'), hold on, scatter(y,x,'filled','r');
+% Load the database SOH
+soh_dir = strcat(strcat('../SOH_save/',keyword),'_harris_sal_hists.mat');
+H = load(soh_dir);
+%score = zeros(length(D),1);
+score_struct = struct();
 
-     h = soh(Ix, Iy, x, y, window_size);
-     SALIENCY_HISTOGRAMS(:,:,i)=h;
-%      norm_thres=0.5;    % can be 0.5 for squared diff or 0.9 of normalized diff
-%      [s] = similarity_score(q_h, h, norm_thres);
-%      score(i-2)=s;
-% %      if s<0.5
-% %          figure,imshow(mat2gray(image)),title('Salient points'), hold on, scatter(cy,cx,'filled','r');
-% %          figure,imshow(mat2gray(image)),title('Found Image');
-%          disp('Score: ');
-%          disp(num2str(s));
-% %      end
+D = dir(strcat(dir_name,'/*.jpg'));
+N=floor(length(D));
+
+for i=1:N
+    h = H.SALIENCY_HISTOGRAMS(:,:,i);
+    [s] = similarity_score(q_h, h, norm_thres);
+    score_struct(i).name = D(i).name;
+    score_struct(i).score = s;
 end
-save('train_harris_sal_maps.mat','SALIENCY_MAPS');
-save('train_harris_sal_masks.mat','ADA_THRES_MASKS');
-save('train_harris_sal_hists.mat','SALIENCY_HISTOGRAMS');
 
+%Sort the scores to get top images
+T = struct2table(score_struct);
+T_sorted = sortrows(T, 'score');
+score_struct_sorted = table2struct(T_sorted);
 
-TEST_ADA_THRES_MASKS=[];
-TEST_SALIENCY_MAPS=[];
-TEST_SALIENCY_HISTOGRAMS=[];
-iter=1;
-for i=N+1:length(D)% for each file in the directory(1 and 2 are '.' and '..')
-     filename = strcat('../../THUR15000/CoffeeMug/Src/',D(i).name);
-     X=double(imread(filename));
-     [im,mask1] = textureDistinctMap(X);
-     if size(im,1)<1000
-         a=1000-size(im,1);
-     end
-     if size(im,2)<1000
-         b=1000-size(im,2);
-     end
-     padded_im=padarray(im,[a/2,b/2],0,'both');
-     padded_mask=padarray(mask1,[a/2,b/2],0,'both');
-     TEST_SALIENCY_MAPS(:,:,iter)=padded_im;
-     TEST_ADA_THRES_MASKS(:,:,iter)=padded_mask;
-     [image, Ix, Iy, x, y] = featureExtraction(double(X),mask1);
-     cx=x;
-     cy=y;
-%      figure,imshow(mat2gray(image)),title('Salient points'), hold on, scatter(y,x,'filled','r');
-
-     h = soh(Ix, Iy, x, y, window_size);
-     TEST_SALIENCY_HISTOGRAMS(:,:,iter)=h;
-%      norm_thres=0.5;    % can be 0.5 for squared diff or 0.9 of normalized diff
-%      [s] = similarity_score(q_h, h, norm_thres);
-%      score(i-2)=s;
-% %      if s<0.5
-% %          figure,imshow(mat2gray(image)),title('Salient points'), hold on, scatter(cy,cx,'filled','r');
-% %          figure,imshow(mat2gray(image)),title('Found Image');
-%          disp('Score: ');
-%          disp(num2str(s));
-% %      end
+for j=1:top_im_num
+    filename = strcat(strcat(dir_name,'/'),score_struct_sorted(j).name);
+    %X = imread(filename);
+    %figure,imshow(X),title('Found Image');
 end
-save('test_harris_sal_maps.mat','TEST_SALIENCY_MAPS');
-save('test_harris_sal_masks.mat','TEST_ADA_THRES_MASKS');
-save('test_harris_sal_hists.mat','TEST_SALIENCY_HISTOGRAMS');
-% score = sum(score,2);
+
+prec = ret_prec(score_struct_sorted, D, top_im_num, keyword, database_dir);
+
+disp('Precision:');
+disp(prec);
+
 toc;
