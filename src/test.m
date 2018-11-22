2ecaf8489517c4473320701cc68c7d16ebf05e96
Krunal Parmar <parmarkrunal2007@gmail.com>
2018-11-23 00:15:05 +0530

    Retrieval result code

--- src/test.m
+++ src/test.m
@@ -30,6 +30,7 @@ H = load(soh_dir);
 %score = zeros(length(D),1);
 score_struct = struct();
 
+dir_name1=strcat('../../../THUR15000/',keyword,'/Src/');
 D = dir(strcat('../../../THUR15000/',keyword,'/Src/*.jpg'));
 N=floor(length(D));
 
@@ -46,9 +47,13 @@ T_sorted = sortrows(T, 'score');
 score_struct_sorted = table2struct(T_sorted);
 
 for j=1:top_im_num
-    filename = strcat(strcat(dir_name,'/'),score_struct_sorted(j).name);
-    %X = imread(filename);
-    %figure,imshow(X),title('Found Image');
+    filename = strcat(strcat(dir_name1,'/'),score_struct_sorted(j).name);
+    X = imread(filename);
+    if j<26
+        subplot(5,5,j);
+        imshow(mat2gray(X));
+    end
+%     figure,imshow(X),title('Found Image');
 end
 
 prec = ret_prec(score_struct_sorted, D, top_im_num, keyword, '../../../THUR15000/');
